4a7b682f1196ee175b07195f9a4f60eb96868df6
Kerry M Collier <kcollier@MacBook-Pro.local>
2016-10-31 19:16:50 -0400

    Gallery Fix
    Added search box interactivity

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
+
+//Header Sticker plugin
 $("#sticker").sticky({topSpacing:0});
 
-
+//Photobox plugin
 $('#gallery').photobox('a',{
     time:0,
-    thumbs: false
     });
+
+//Search Box
+$("#search").keyup(function(){
+    // Retrieve the input field text
+    var filter = $(this).val();
+    // Loop through the comment list
+    $("#gallery img").each(function(){
+        // If the list item does not contain the title attr, fade it out
+        if ($(this).attr("alt").search(new RegExp(filter, "i")) < 0) {
+            $(this).fadeOut();
+        // Show the list item if the phrase matches
+        } else {
+            $(this).fadeIn();
+        }
+    });
+});
